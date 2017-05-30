cf2288330fb77db59ad9994881cb7ad3d104d355
Thomas Cheung <cheungth25@gmail.com>
2017-05-30 17:26:02 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,11 +3,11 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each { |key1, value1|
     value1.each { |key2, value2|
-      new_hash[key2] = value2
-      if new_hash[key2][:style]
+      if new_hash[key2] == nil
+        new_hash[key2] = value2
         new_hash[key2][:style] = [key1]
       else
-        new_hash[key2][:style] << [key1]
+        new_hash[key2][:style] << key1
       end
     }
   }
